5d8c860c43a33971bb87bda28a2e54d29470f7f7
Benzarti <121379135+Benzarti-Ahmed-Yassine@users.noreply.github.com>
2025-03-29 07:40:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def calculate():
         "gridTemperatures": grid_temperatures.tolist()
     })
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 4500))
+    app.run(host='0.0.0.0', port=port, debug=False)
